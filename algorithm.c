83398525e0bf8805289ce4fbf9aba1f442d09641
Dominika <dominika.golebiewska2.stud@pw.edu.pl>
2022-03-24 17:33:17 +0100

    Dodanie przeszukiwania wszerz

--- algorithm.c
+++ algorithm.c
@@ -1,6 +1,33 @@
 #include "graph.h"
 #include "queue.h"
 
-int checkIntegrity( node_t graph ); //BFS
-
-void findPath( node_t graph ); //Dijsktra
+int checkIntegrity( graph_t graph ) { //BFS
+    q_t working, checked = NULL;
+    node_t head = graph.head;
+    working = initQueue( head->id );
+    int i, count = 0, nodes = graph.width*graph.len;
+    while( working != NULL ) {
+	for( i= 0; i < head->ways; i++ ) 
+	    if( !inQueue( working, head->conn[i]->id ) && !inQueue( checked, head->conn[i]->id ) ) //dodanie do kolejki wezlow ktore nie zostaly jeszcze odwiedzone
+		addToQueue( working, head->conn[i]->id );
+	if( checked == NULL ) 
+	    checked = initQueue( popFromQueue( &working ) );
+	else
+	    addToQueue( checked, popFromQueue( &working ) );
+	if( working != NULL )
+	    for( i= 0; i < head->ways; i++ ) 
+		if( head->conn[i]->id == working->val ) {
+		    head = head->conn[i];
+		    break;
+		}
+    }
+    while( checked != NULL ) {
+	count++;
+	checked = checked->next;
+    }
+    freeQueue( checked );
+    if( count == nodes )
+	return 1;
+    return 0;
+}
+void findPath( graph_t graph ); //Dijsktra

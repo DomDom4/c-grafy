53cba7a7497569680867bda19305e6ac12d76608
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-04 11:16:55 +0200

    Update algorithm.c
    Dodanie algorytmu Dijkstry, funkcji pomocniczych wypełniających tablice funkcji findPath. Dodanie przyjmowanych parametrów (numeru węzła startowego i kończącego szukanej ścieżki, wartość max przyjmowanych wag, aby obliczyć największą możliwą długość ścieżki). Zamiana void na path (strukturę przechowującą tablicę ze wskaźnikami kolejnych węzłów najkrótszej ścieżki i wartość sumy wag przejść tej ścieżki)

--- algorithm.c
+++ algorithm.c
@@ -31,4 +31,96 @@ int checkIntegrity( graph_t graph ) { //BFS
 	return 1;
     return 0;
 }
-void findPath( graph_t graph ); //Dijsktra
+
+path findPath( graph_t graph, int start, int end, int maxval ) { //Dijsktra
+	int gsize = graph->width * graph->len, i, k = 0;
+
+        q_t Q = malloc(gsize *sizeof(node_t));
+
+        int Qd = gsize;
+
+        double *d = malloc(gsize *sizeof(double));
+        node_t *p = malloc(gsize *sizeof(node_t));
+        node_t *temp = malloc(gsize *sizeof(node_t));
+
+        node_t c = findNode(graph, start);
+        path dp = malloc(sizeof(path));
+
+        Q = priorityQueue(graph, c); 
+        //writeQueue(Q, gsize);
+
+        infinity(d, gsize, maxval);
+        null(p, gsize);
+
+        d[start] = 0;
+    
+        while(Qd > 0){ 
+                if((c = popFromQueue(&Q)) == NULL){
+                        printf("Blad");
+                        break;
+                }
+                //printf("%d -> ", c->id);
+                for(i=0; i<c->ways; i++){
+                        if(d[c->conn[i]->id] > d[c->id]+c->val[i]){
+                                d[c->conn[i]->id] = d[c->id] + c->val[i];
+                                p[c->conn[i]->id] = c;
+                        }
+                }
+                Qd--;
+        }
+    
+/*
+        -- Sprawdzenie przejść --
+
+        int tmp = end;
+        printf("\nPrzejscia od ostatniego: ");
+        while(tmp != start){
+                printf("%d <- ", tmp);
+                tmp = p[tmp]->id;
+        }
+        printf("%d; ", tmp);
+
+	printf("Suma wag przejsc: %lf\n", d[end]);
+*/
+
+        dp->val_s = d[end];
+
+        temp[k] = findNode(graph, end);
+        k++;
+
+        while( end != start ){
+                temp[k] = p[end];
+                end = p[end]->id;
+                k++;
+        }
+
+        dp->nodes = malloc(k*sizeof(node_t));
+
+        for(i=0; i<k; i++)
+                dp->nodes[i] = temp[k-i-1];
+
+        dp->n_nb = k;
+
+
+        free(p);
+        free(temp);
+        free(d);
+
+        return dp;
+}
+
+void infinity(double *d, int s, int n)
+{
+        int i;
+
+        for(i=0; i<s; i++)
+                d[i]=2*s*n;
+}
+
+void null(node_t *t, int n)
+{
+        int i;
+
+        for(i=0; i<n; i++)
+                t[i]=NULL;
+}	

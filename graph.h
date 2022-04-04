f5395c7a444b43275ac55542bb7b2db0e10863c9
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-04 11:42:11 +0200

    Dodanie struktury path, którą zwraca funkcja findPath, dodanie funkcji

--- graph.h
+++ graph.h
@@ -18,9 +18,17 @@ typedef struct graph {
     int len;
 } graph_t;
 
+typedef struct{
+        node_t *nodes;
+        int n_nb;
+        double val_s;
+} *path;
+
 graph_t genFromParams( int x, int y, double a, double b ); //Generowanie grafu - szerokosc, wysokos, dolna waga, gorna waga
 
 node_t makeNode( int width, int len, int current );
+
+node_t fileMakeNode( int ways);
 
 graph_t readFromFile( FILE *in ); //Czytanie z pliku
 
@@ -28,10 +36,18 @@ void printToFile( graph_t graph, FILE *out ); //Zapisanie do pliku
 
 void divideGraph( graph_t *graph ); //Rozdzielanie na graf niespojny
 
+node_t findNode(graph_t *graph, int n );
+
 void freeGraph( graph_t graph );
 
 int checkIntegrity( graph_t graph ); //BFS
 
-void findPath( graph_t graph ); //Dijsktra
+path findPath( graph_t *graph, int start, int end, int maxval ); //Dijsktra
+
+void writeGraph(graph_t *graph);
+
+void infinity(double *d, int s, int n); 
+
+void null(node_t *t, int n); 
 
 #endif

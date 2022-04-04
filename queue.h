d31b36e341cae88ffbe5415321663df7742f9e01
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-04 11:56:23 +0200

    Update queue.h
    Zamiana typu zminnej przechowywanej w strukturze z int val na node_t node (aby przechowywało wskaźnik na węzeł a nie tylko jego numer), dodanie biblioteki graph.h

--- queue.h
+++ queue.h
@@ -3,20 +3,25 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "graph.h"
 
-typedef struct q {
-    int val;
+typedef struct q { 
+    node_t node;
     struct q *next;
 } *q_t;
 
-q_t initQueue( int value );
+q_t initQueue( node_t n );
 
-void addToQueue( q_t q, int value );
+void addToQueue( q_t q, node_t n );
 
-int popFromQueue( q_t *q );
+node_t popFromQueue( q_t *q );
 
 int inQueue( q_t q, int value );
 
-void freeQueue( q_t q );
+q_t priorityQueue( graph_t *graph, node_t n );
+
+void writeQueue(q_t q, int n); 
+
+void freeQueue( q_t q )
 
 #endif

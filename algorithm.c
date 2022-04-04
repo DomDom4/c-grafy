dea95000bc2969e0d61b6f9b69a29f0b0ba19076
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-04 12:06:00 +0200

    Poprawienie BFS z powodu zmiany struktury kolejki

--- algorithm.c
+++ algorithm.c
@@ -4,33 +4,34 @@
 int checkIntegrity( graph_t graph ) { //BFS
     q_t temp, working, checked = NULL;
     node_t head = graph.head;
-    working = initQueue( head->id );
+    working = initQueue( head );
     int i, count = 0, nodes = graph.width*graph.len;
-    while( working != NULL ) {
-	for( i= 0; i < head->ways; i++ ) 
-	    if( !inQueue( working, head->conn[i]->id ) && !inQueue( checked, head->conn[i]->id ) ) //dodanie do kolejki wezlow ktore nie zostaly jeszcze odwiedzone
-		addToQueue( working, head->conn[i]->id );
-	if( checked == NULL ) 
-	    checked = initQueue( popFromQueue( &working ) );
-	else
-	    addToQueue( checked, popFromQueue( &working ) );
-	if( working != NULL )
-	    for( i= 0; i < head->ways; i++ ) 
-		if( head->conn[i]->id == working->val ) {
-		    head = head->conn[i];
-		    break;
-		}
-    }
+    while( working != NULL ) { 
+        for( i= 0; i < head->ways; i++ ) 
+            if( !inQueue( working, head->conn[i]->id ) && !inQueue( checked, head->conn[i]->id ) ) //dodanie do kolejki wezlow ktore nie zostaly jeszcze odwiedzone
+                addToQueue( working, head->conn[i] );
+        if( checked == NULL ) 
+            checked = initQueue( popFromQueue( &working ) );
+        else
+            addToQueue( checked, popFromQueue( &working ) );
+        if( working != NULL )
+            for( i= 0; i < head->ways; i++ ) 
+                if( head->conn[i]->id == working->node->id ) { 
+                    head = head->conn[i];
+                    break;
+                }
+    }   
     temp = checked;
-    while( checked != NULL ) {
-	count++;
-	checked = checked->next;
-    }
+    while( checked != NULL ) { 
+        count++;
+        checked = checked->next;
+    }   
     freeQueue( temp );
     if( count == nodes )
-	return 1;
+        return 1;
     return 0;
 }
+
 
 path findPath( graph_t graph, int start, int end, int maxval ) { //Dijsktra
 	int gsize = graph->width * graph->len, i, k = 0;

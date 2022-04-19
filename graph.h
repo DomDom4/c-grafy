b6e7fb7828720ffa2912275911008121bf5fe28a
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-19 17:10:42 +0200

    Dodanie funkcji dzielenia

--- graph.h
+++ graph.h
@@ -13,9 +13,10 @@ typedef struct node {
 } *node_t;
 
 typedef struct graph {
-    node_t head;
-    int width;
-    int len;
+    node_t *head;
+    int *width;
+    int *len;
+    int n;
 } graph_t;
 
 typedef struct{
@@ -23,6 +24,8 @@ typedef struct{
         int n_nb;
         double val_s;
 } *path;
+
+void makeGraph(graph_t *graph, node_t head, int w, int l);
 
 graph_t genFromParams( int x, int y, double a, double b ); //Generowanie grafu - szerokosc, wysokos, dolna waga, gorna waga
 
@@ -34,15 +37,27 @@ double readFromFile( graph_t *graph, FILE *in ); //Czytanie z pliku
 
 void printToFile( graph_t graph, FILE *out ); //Zapisanie do pliku
 
-node_t findNode(graph_t *graph, int n );
+node_t findNode(graph_t *graph, int n, int nn );
 
-void freeGraph( graph_t graph );
+void freeGraph( graph_t graph, int nn );
 
-int checkIntegrity( graph_t graph ); //BFS
+int checkIntegrity( graph_t graph, int nn ); //BFS
 
 path findPath( graph_t *graph, int start, int end, double maxval ); //Dijsktra
 
-void writeGraph(graph_t *graph, FILE *out);
+void divideGraph(graph_t *graph, int n); 
+
+int wasDrawn(int *tab, int n, int a); 
+
+void deleteConn(node_t node, int n); 
+
+int findWidth(graph_t *graph, int n, int a, int gwidth);
+
+void graphAdd(graph_t *graph, node_t node, int w, int l); 
+
+void graphChangeSize(graph_t *graph, int n, int w, int l, int gpw);
+
+void writeGraph(graph_t *graph, FILE *out, int nn);
 
 int findConnIndex(node_t node, int a);
 

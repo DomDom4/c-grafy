301c5dc3b8135308c61aba571d91651f8d86bb71
plaskonp <102331778+plaskonp@users.noreply.github.com>
2022-04-19 16:44:31 +0200

    Dodanie obsługiwania podzielonych grafu
    Dodanie funkcji pathExistence()

--- algorithm.c
+++ algorithm.c
@@ -2,10 +2,10 @@
 #include "queue.h"
 #include "error.h"
 
-int checkIntegrity( graph_t graph ) { //BFS
+int checkIntegrity( graph_t graph, int nn ) { //BFS
     q_t temp, working = NULL, checked = NULL;
     node_t head = graph.head;
-    int i, count = 0, nodes = graph.width*graph.len;
+    int i, count = 0, nodes = graph.width[nn]*graph.len;
     addToQueue( &working, head );
     while( working != NULL ) { 
         for( i= 0; i < head->ways; i++ ) 
@@ -26,19 +26,64 @@ int checkIntegrity( graph_t graph ) { //BFS
     return 0;
 }
 
+int pathExistence(graph_t *graph, int a, int b, int n, int gwidth){
+        int i, j, ta = 0, tb = 0, tmpi;
+        node_t node = graph->head[n], tmp;
+
+        for(i=0; i<graph->len[n]; i++){
+                tmp = node;
+                for(j=0; j<graph->width[n]; j++){
+                        if(node->id == a)
+                                ta = 1; 
+                        if(node->id == b)
+                                tb = 1; 
+                        if((tmpi = findConnIndex(node, node->id+1)) >= 0)
+                                node = node->conn[tmpi];        
+                }
+                if((tmpi = findConnIndex(tmp, tmp->id+gwidth)) >= 0)
+                        node = tmp->conn[tmpi];
+        }
+
+        if(ta == 1 && tb ==1)
+                return 1; 
+        else
+                return 0;
+}
 
 path findPath( graph_t *graph, int start, int end, double maxval ) { //Dijsktra
-	int gsize = graph->width * graph->len, i, k = 0;
+	int i, n = -1, gwidth = 0;
 
-        q_t Q = malloc(gsize *sizeof(node_t));
+       for(i=0; i<graph->n; i++){
+                gwidth += graph->width[i];
+        }
 
-        int Qd = gsize;
+	if(graph->n != 0){
+		for(i=0; i<graph->n; i++){
+			if(pathExistence(graph, start, end, i, gwidth) == 1){
+				n = i;
+				break;
+			}
+		}
 
+		if(n < 0){
+			printf("NO_PATH\n");
+			exit( NO_PATH );
+		}
+	} else {
+		n = 0;	
+	}
+
+        int gsize = gwidth * graph->len[0], ngsize = graph->width[n] * graph->len[n], k = 0;
+
+        q_t Q = malloc(ngsize *sizeof(node_t));
+
+        int Qd = ngsize;
+	
         double *d = malloc(gsize *sizeof(double));
         node_t *p = malloc(gsize *sizeof(node_t));
         node_t *temp = malloc(gsize *sizeof(node_t));
 
-        node_t c = findNode(graph, start);
+        node_t c = findNode(graph, start, );
         path dp = malloc(sizeof(path));
 
 	if((Q == NULL) || (d == NULL) || (p == NULL) || (temp == NULL) || (dp == NULL)){
@@ -83,7 +128,7 @@ path findPath( graph_t *graph, int start, int end, double maxval ) { //Dijsktra
 
         dp->val_s = d[end];
 
-        temp[k] = findNode(graph, end);
+        temp[k] = findNode(graph, end, n);
         k++;
 
         while( end != start ){
@@ -95,7 +140,7 @@ path findPath( graph_t *graph, int start, int end, double maxval ) { //Dijsktra
         dp->nodes = malloc(k*sizeof(node_t));
 
 	if(dp->nodes == NULL){
-        printf("NOT_ENOUGH_MEMORY\n");
+        	printf("NOT_ENOUGH_MEMORY\n");
                 exit( NOT_ENOUGH_MEMORY );
         }
 	
@@ -103,7 +148,6 @@ path findPath( graph_t *graph, int start, int end, double maxval ) { //Dijsktra
                 dp->nodes[i] = temp[k-i-1];
 
         dp->n_nb = k;
-
 
         free(p);
         free(temp);
